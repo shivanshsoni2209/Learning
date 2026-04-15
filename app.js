8c49245b93bc20e3d7906bef09c039d67b958f35
Shivansh Soni <shivanshsoni2209@gmail.com>
2026-04-16 02:44:27 +0530

    Made changes in app.js in learining git repo

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 console.log(
-  "App started very long ago and is still running for a very long time and will continue to run for a very long time",
+  "App started very long ago and is still running for a very long time and will continue to run for a very long time and this is just a test to see how long it can run and if it can run indefinitely without crashing or running out of memory or any other issues that may arise from running for such a long time.",
 );
